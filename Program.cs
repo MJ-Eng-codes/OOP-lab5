f884052e390559736fd2d71928e3484ee89f1284
swany <swany@MJ>
2024-10-17 20:05:23 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace OOP_lab5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle1 = new Circle(5);
+            Console.WriteLine($"Arean av circle 1 är: {circle1.GetArea()}");
+        }
+    }
+
+    public class Circle
+    {
+        //Attribute
+        private int radius = 1;
+
+        //Constructor
+        public Circle(int Radius)
+        {
+            radius = Radius;
+        }
+
+        //Method
+        public double GetArea()
+        {
+            double Area = (radius * radius * Math.PI);
+            return Area;
+        }
+        
+    }
+}
